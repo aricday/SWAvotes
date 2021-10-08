a208db87793b67b238d0432c1e0151854524595e
Aric Day <aricwday@gmail.com>
2021-10-08 13:42:24 -0500

    Update voteResults.sh

--- voteResults.sh
+++ voteResults.sh
@@ -4,10 +4,10 @@ node Votes.js
 
 a=1;
 
-while [ $a -lt 10 ]
+while [ $a -lt 16 ]
 do
 	echo "Results for Team #"$a
-    sort -u -t"|" -k 1,1  vote.txt | awk -F"|" '$2==$a' | wc -l
+    sort -u -t"|" -k 1,1  vote.txt | awk -F"|" -v TEAM=$a '$2==TEAM' | wc -l
     echo "***************"
     a=`expr $a + 1`
 done
